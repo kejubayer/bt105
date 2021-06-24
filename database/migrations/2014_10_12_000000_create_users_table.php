75366af8a18086a07d04416911f90429f1c4f1c4
kejubayer <khondokereftakharjubayer@gmail.com>
2021-06-24 20:37:24 +0600

    factory seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('address')->nullable();
+            $table->string('role');
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
