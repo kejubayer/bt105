40cb205bdd9553e809656faceeeeaca0e7d21102
kejubayer <khondokereftakharjubayer@gmail.com>
2021-07-06 10:56:16 +0600

    user crud

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,30 @@ Route::get('/', function () {
     return view('test');
 });
 
-Route::get('/dashboard',[\App\Http\Controllers\Backend\DashboardController::class,'index'])->name('admin.dashboard');
 
-Route::get('products',[\App\Http\Controllers\Backend\ProductController::class,'index'])->name('admin.product');
+Route::get('admin/dashboard', [\App\Http\Controllers\Backend\DashboardController::class, 'index'])->name('admin.dashboard');
 
-Route::get('products/create',[\App\Http\Controllers\Backend\ProductController::class,'create'])->name('admin.product.create');
+Route::get('admin/products', [\App\Http\Controllers\Backend\ProductController::class, 'index'])->name('admin.product');
 
-Route::post('products/create',[\App\Http\Controllers\Backend\ProductController::class,'store']);
+Route::get('admin/products/create', [\App\Http\Controllers\Backend\ProductController::class, 'create'])->name('admin.product.create');
 
-Route::get('products/edit/{id}',[\App\Http\Controllers\Backend\ProductController::class,'edit'])->name('admin.product.edit');
+Route::post('admin/products/create', [\App\Http\Controllers\Backend\ProductController::class, 'store']);
 
-Route::post('products/edit/{id}',[\App\Http\Controllers\Backend\ProductController::class,'update']);
+Route::get('admin/products/edit/{id}', [\App\Http\Controllers\Backend\ProductController::class, 'edit'])->name('admin.product.edit');
 
-Route::get('products/delete/{id}',[\App\Http\Controllers\Backend\ProductController::class,'delete'])->name('admin.product.delete');
+Route::post('admin/products/edit/{id}', [\App\Http\Controllers\Backend\ProductController::class, 'update']);
+
+Route::get('admin/products/delete/{id}', [\App\Http\Controllers\Backend\ProductController::class, 'delete'])->name('admin.product.delete');
+
+Route::get('admin/users',[\App\Http\Controllers\Backend\UserController::class,'index'])->name('admin.user');
+
+Route::get('admin/users/create',[\App\Http\Controllers\Backend\UserController::class,'create'])->name('admin.user.create');
+
+Route::post('admin/users/create',[\App\Http\Controllers\Backend\UserController::class,'store']);
+
+Route::get('admin/users/edit/{id}',[\App\Http\Controllers\Backend\UserController::class,'edit'])->name('admin.user.edit');
+
+Route::post('admin/users/edit/{id}',[\App\Http\Controllers\Backend\UserController::class,'update']);
+
+Route::get('admin/users/delete/{id}',[\App\Http\Controllers\Backend\UserController::class,'delete'])->name('admin.user.delete');
+
