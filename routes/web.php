394a88bf6dbb954bea532c0591b82aa04465426c
kejubayer <khondokereftakharjubayer@gmail.com>
2021-08-02 21:09:24 +0600

    cart

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::post('register', [\App\Http\Controllers\Frontend\LoginController::class, 
 Route::get('login', [\App\Http\Controllers\Backend\LoginController::class, 'login'])->name('login');
 Route::post('login', [\App\Http\Controllers\Backend\LoginController::class, 'doLogin']);
 
+//card
+Route::get('add/cart/{id}',[\App\Http\Controllers\Frontend\CartController::class,'addToCart'])->name('add.cart');
+Route::get('cart/',[\App\Http\Controllers\Frontend\CartController::class,'showCart'])->name('show.cart');
+
 Route::middleware('auth')->group(function () {
 
     Route::get('profile',[\App\Http\Controllers\Frontend\LoginController::class,'profile'])->name('profile');
