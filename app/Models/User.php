596435c746ac30e99e63a9992a7f5e9119ae0bd6
kejubayer <khondokereftakharjubayer@gmail.com>
2021-08-05 20:44:27 +0600

    relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -16,6 +15,11 @@ class User extends Authenticatable
      *
      * @var array
      */
-  protected $guarded=[];
+    protected $guarded = [];
+
+    public function order()
+    {
+        return $this->hasMany(Order::class,'user_id','id');
+    }
 
 }
